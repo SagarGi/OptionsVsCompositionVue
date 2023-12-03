687a75b2431fc8f27806aacfe8cc8e609ac7c5f5
sagar <sagargurung1001@gmail.com>
2023-12-03 16:16:22 +0545

    Added simple demo for options vs composition

---
+++ src/components/CompositionAPIDemo.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <p>Hello</p>
+  <h1>{{ msg }}</h1>
+  <p>{{name}}</p>
+</template>
+<script>
+import {ref, reactive, toRefs} from "vue";
+export default {
+  props: {
+    msg: String
+  },
+  setup(props) {
+    const { msg } = props
+    const name = ref('Sagar Gurung')
+    return {
+      name,
+      msg
+    }
+  }
+}
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/OptionAPIDemo.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <p>Hello</p>
+  <h1>{{ msg }}</h1>
+  <p>{{this.name}}</p>
+</template>
+<script>
+  export default {
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        name: 'Sagar Gurung',
+      }
+    },
+    methods() {
+
+    },
+    computed: {
+
+    },
+    watch() {
+
+    }
+  }
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
